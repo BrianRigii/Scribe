8af2d6b097f2fb7795daf8dd848bb1730fc9744f
brian <johnbryanjb011@gmail.com>
2025-11-24 11:15:27 +0300

    setup cloud function

--- functions/process-book/lib/main.dart
+++ functions/process-book/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 
@@ -7,31 +8,25 @@ Future<dynamic> main(final context) async {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   final client = Client()
-    .setEndpoint(Platform.environment['APPWRITE_FUNCTION_API_ENDPOINT'] ?? '')
-    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'] ?? '')
-    .setKey(context.req.headers['x-appwrite-key'] ?? '');
-  final users = Users(client);
+      .setEndpoint(Platform.environment['APPWRITE_URL'] ?? '')
+      .setProject(Platform.environment['APPWRITE_PROJECT_ID'] ?? '')
+      .setKey(context.req.headers['x-appwrite-key'] ?? '');
 
   try {
-    final response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    context.log('Total users: ' + response.total.toString());
+    Map<String, dynamic> body = jsonDecode(context.req.body);
+    String fileId = body['fileId'];
+    Storage storage = Storage(client);
+    final file = await storage.getFile(
+      bucketId: Platform.environment['BOOKS_BUCKET_ID'] ?? '',
+      fileId: fileId,
+    );
+    context.log('Processing file: ${file.name} with ID: ${file.$id}');
+    // Add your book processing logic here
+    context.res = {
+      'status': 200,
+      'body': 'Book processing started for file ID: $fileId',
+    };
   } catch (e) {
-    context.error('Could not list users: ' + e.toString());
+    context.error('Error processing book: $e');
   }
-
-  // The req object contains the request data
-  if (context.req.path == "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return context.res.text('Pong');
-  }
-
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
 }
